4c09ef9933676628683d4aae34df5e9c7a914d7e
mazurek321 <mazurek321.93@wp.pl>
2026-07-14 10:47:21 +0200

    Deleted unnecessary files, .env added, security upgrade

--- src/Services.Auth/HybridShop.Services.Auth.Api/Program.cs
+++ src/Services.Auth/HybridShop.Services.Auth.Api/Program.cs
@@ -2,9 +2,14 @@ using HybridShop.Services.Auth.Application;
 using HybridShop.Services.Auth.Infrastructure;
 using HybridShop.BuildingBlocks.OpenApi;
 using HybridShop.BuildingBlocks.OpenApi.Auth;
+using dotenv.net;
 
+
+DotEnv.Load();
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddEnvironmentVariables();
 
 builder.Services.AddSharedOpenApi();
 builder.Services.AddAuthServices(builder.Configuration);
